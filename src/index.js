410dedc5e2b7d5e2174b7507dcf8591dc76bd628
Nilanth <nilanth@mallow-tech.com>
2021-06-06 11:41:36 +0530

    init create-react-library@3.1.1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import styles from './styles.module.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export const ExampleComponent = ({ text }) => {
+  return <div className={styles.test}>Example Component: {text}</div>
+}
